bc867a044ff299d0c06a80b0fce06bc419497112
afonsopacifer <afonsopolis@gmail.com>
2022-08-20 04:46:43 -0300

    Fixes and Deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b deploy
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:NeowayLabs/neowaylabs.github.io.git deploy
+
+cd -
